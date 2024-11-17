8573532dfed0dd6fdf2a51ff0c4c1f72728e029f
zannazw <zanna.wahedi@outlook.com>
2024-11-17 18:55:29 +0100

    add service tests

--- src/test/java/com/zannazw/todo_app/service/ToDoServiceTest.java
+++ src/test/java/com/zannazw/todo_app/service/ToDoServiceTest.java
@@ -9,9 +9,10 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -24,24 +25,27 @@ class ToDoServiceTest {
     private ToDoRepository todoRepository;
 
     @Test
-    void createToDoEquals() {
+    void createToDo_Equals() {
+        ToDo input = new ToDo("Reinigen", "Pfanne");
         ToDo expectedResult = new ToDo("Reinigen", "Pfanne");
 
-        when(todoRepository.save(expectedResult)).thenReturn(expectedResult);
-        ToDo actualResult = todoService.createToDo(new ToDo("Reinigen", "Pfanne"));
+        when(todoRepository.save(input)).thenReturn(expectedResult);
+        ToDo actualResult = todoService.createToDo(input);
 
         assertEquals(expectedResult, actualResult);
+        verify(todoRepository).save(input);
     }
 
     @Test
-    void createToDoNotEquals() {
-        ToDo expectedResult = new ToDo("Reinigen", "Pfanne");
-        ToDo actualInput = new ToDo("Reinigen", "Topf");
+    void createTo_DoNotEquals() {
+        ToDo input = new ToDo("Reinigen", "Pfanne");
+        ToDo expectedResult = new ToDo("Reinigen", "Topf");
 
-        when(todoRepository.save(expectedResult)).thenReturn(actualInput);
-        ToDo actualResult = todoService.createToDo(actualInput);
+        when(todoRepository.save(input)).thenReturn(expectedResult);
+        ToDo actualResult = todoService.createToDo(input);
 
         assertNotEquals(expectedResult, actualResult);
+        verify(todoRepository).save(input);
     }
 
     @Test
@@ -60,17 +64,47 @@ class ToDoServiceTest {
 
     @Test
     void getToDoById() {
+        int input = 3;
+        Optional<ToDo> expectedResult = Optional.of(new ToDo("Waschen", "Bettbezug"));
+
+        when(todoRepository.findById(input)).thenReturn(expectedResult);
+        Optional<ToDo> actualResult = todoService.getToDoById(input);
+
+        assertEquals(expectedResult, actualResult);
     }
 
     @Test
     void getToDoByTitle() {
+        String input = "Kochen";
+        Optional<ToDo> expectedResult = Optional.of(new ToDo("Kochen", "Suppe"));
+
+        when(todoRepository.findByTitle(input)).thenReturn(expectedResult);
+        Optional<ToDo> actualResult = todoService.getToDoByTitle(input);
+
+        assertTrue(actualResult.isPresent());
+        assertEquals(expectedResult, actualResult);
     }
 
     @Test
     void updateToDoById() {
+        int input = 3;
+        ToDo newTodo = new ToDo("Kochen", "Kartoffel-Suppe");
+
+        ToDo oldTodo = new ToDo("Kochen", "Suppe");
+        Optional<ToDo> expectedResult = Optional.of(oldTodo);
+
+        when(todoRepository.findById(input)).thenReturn(expectedResult);
+        when(todoRepository.save(oldTodo)).thenReturn(oldTodo);
+
+        Optional<ToDo> actualResult = todoService.updateToDoById(input, newTodo);
+
+        assertEquals(expectedResult, actualResult);
     }
 
     @Test
     void deleteToDoById() {
+        int input = 3;
+        todoService.deleteToDoById(input);
+        verify(todoRepository).deleteById(input);
     }
 }
